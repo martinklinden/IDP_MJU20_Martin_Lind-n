09fce0f93a19d22a707beb1399bc676130a24d3f
Martin Lindén <MaritnI@hotmail.com>
2020-10-27 12:08:26 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IDP_MJU20_Martin_Lindén
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
