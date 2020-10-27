e0abe6044d26277bb85d53efb6c35c1d37f9080e
Martin Lindén <MaritnI@hotmail.com>
2020-10-27 12:11:18 +0100

    Added instructions for Level 1

--- Program.cs
+++ Program.cs
@@ -6,7 +6,22 @@ namespace IDP_MJU20_Martin_Lindén
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+             Du ska utgå från att användaren anger tolv siffror så du behöver inte ha någon form av
+             felhantering för detta.
+             Programmet frågar användaren efter ett 12-siffrigt personnummer YYYYMMDDnnnc –
+             användaren ska skriva in hela numret och får inte dela upp det – och kontroller om det är
+             korrekt uppbyggt enligt följande:
+             1. Rätt antal siffror dvs 12
+             2. Årtalet ska vara från 1753 till och med 2020 (Sverige bytte kalender 1753)
+             3. Månaden ska var giltig 1 – 12
+             4. Dagen ska vara giltig och kontrolleras mot månaden (se nedan)
+             5. Födelsenumret nnn ska vara 000 – 999
+             6. Kontrollera kön. Födelsenumret är udda för män och jämnt för kvinnor
+             (OBS! 0 betraktas i detta fall som jämn dvs kvinna)
+             7. Programmet ska skriva ut på skärmen om personnumret är korrekt och om
+             personen är man eller kvinna (juridisk).
+             */
         }
     }
 }
